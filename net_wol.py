362f5be7901ea9ad1dcced3db15af5a61f96e56a
CoolPolyester <Mkasper045@gmail.com>
2023-05-04 01:08:36 -0700

    Add wifi connecting in connect method

--- net_wol.py
+++ net_wol.py
@@ -1,5 +1,8 @@
 import csv
 
+import network
+
+
 def connect():
     # Open credentials.csv that stores network information
     with open("credentials.csv") as credentials:
@@ -8,3 +11,8 @@ def connect():
         # Parse network credentials from csv
         ssid = csv_reader[0][0]
         password = csv_reader[0][1]
+
+        # Establish connection to router
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(str(ssid), str(password))
