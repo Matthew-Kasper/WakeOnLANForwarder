457317732bad7513079db29ffe571710a91a29ed
CoolPolyester <Mkasper045@gmail.com>
2023-05-04 01:41:28 -0700

    Switch credentials to use txt instead of csv

--- net_wol.py
+++ net_wol.py
@@ -1,18 +1,15 @@
-import csv
-
 import network
-
 
 def connect():
     # Open credentials.csv that stores network information
-    with open("credentials.csv") as credentials:
-        csv_reader = csv.reader(credentials, delimiter=',')
+    credentials = open("credentials.txt", "r")
+    lines = credentials.readlines()
 
-        # Parse network credentials from csv
-        ssid = csv_reader[0][0]
-        password = csv_reader[0][1]
+    # Takes first and second line of credentials file and takes the ssid and password from them
+    ssid = lines[0].strip()
+    password = lines[1].strip()
 
-        # Establish connection to router
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        wlan.connect(str(ssid), str(password))
+    # Connect to the network using ssid and password
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(str(ssid), str(password))
